9457b651cf0941b0512af83302f94aa927c8a2d1
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-12-25 21:22:04 +0800

    Add default echo bot

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import linebot from 'linebot'
+
+const PORT = 6459;
+
+dotenv.config();
+
+const app = express();
+const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+bot.on('message', (event) => {
+    event.reply(event.message.text)
+});
+
+app.post('/', bot.parser());
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
