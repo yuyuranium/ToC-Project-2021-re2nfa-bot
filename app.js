7e2fcc98d1cf952c8a83de0f57ce1d30f65bc842
yuyuranium <e24076459@gs.ncku.edu.tw>
2021-12-29 23:00:56 +0800

    Add image uploading to imgur
    Add db/tmp/ directory to store the rendered output png file. Add a
    helper function to upload image to imgur. Remove unused packages.

--- app.js
+++ app.js
@@ -4,19 +4,43 @@ const linebot = require('linebot');
 const reParser = require('./util/re-parser');
 const visualize = require('javascript-state-machine/lib/visualize');
 const { CliRenderer } = require('@diagrams-ts/graphviz-cli-renderer');
+const axios = require('axios')
+const fs = require('fs');
 
 const PORT = 6459;
+const OUTPUT_FILE = './db/tmp/output.png';
 
 dotenv.config();
 
+const render = CliRenderer({ outputFile: OUTPUT_FILE, format: 'png' });
+
 const app = express();
+
 const bot = linebot({
   channelId: process.env.CHANNEL_ID,
   channelSecret: process.env.CHANNEL_SECRET,
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
 
-const render = CliRenderer({ outputFile: 'test.png', format: 'png' });
+const upload2Imgur = async function(filename) {
+  try {
+    let image = fs.readFileSync(filename, { encoding: 'base64' });
+    let options = {
+      'method': 'POST',
+      'url': 'https://api.imgur.com/3/image',
+      'headers': {
+        'Authorization': `Client-ID ${process.env.CLIENT_ID}`
+      },
+      'data': {
+        image: image
+      }
+    };
+    let res = await axios(options);
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 bot.on('message', async (event) => {
   try {
@@ -25,11 +49,22 @@ bot.on('message', async (event) => {
     let dotScript = visualize(fsm, { orientation: 'horizontal' })
     await render(dotScript);
 
-    let reply = '';
+    let edges = '';
     for (edge of nfa.edges) {
-      reply += `${edge.from} -${edge.name}> ${edge.to}\n`;
+      edges += `${edge.from} -${edge.name}> ${edge.to}\n`;
     }
-    event.reply(reply.slice(0, -1));
+    let res = await upload2Imgur(OUTPUT_FILE);
+    event.reply([
+      {
+        type: 'text',
+        text: edges.slice(0, -1)
+      },
+      {
+        type: 'image',
+        originalContentUrl: res.data.link,
+        previewImageUrl: res.data.link
+      }
+    ]);
   } catch (e) {
     console.log(e);
     event.reply(reParser.getErrorMessage(e));
