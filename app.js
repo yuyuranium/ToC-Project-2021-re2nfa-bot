b30330c05c8f36e8a611debb72ee0fb5be30e716
yuyuranium <e24076459@gs.ncku.edu.tw>
2022-01-01 17:29:00 +0800

    Improve chat UI

--- app.js
+++ app.js
@@ -41,12 +41,14 @@ const ControlFsm = StateMachine.factory({
   transitions: [
     { name: 're2nfa', from: '*', to: 'waitingReInput' },
     { name: 'help', from: '*', to: 'waitingHelpType' },
-    { name: 'correctReInput', from: 'waitingReInput', to: 'gotNfa' },
-    { name: 'incorrectReInput', from: 'waitingReInput', to: 'waitingReInput' },
+    { name: 'reInput', from: 'waitingReInput', to: 'hasReInput' },
+    { name: 'regret', from: 'hasReInput', to: 'waitingReInput' },
+    { name: 'correctReInput', from: 'hasReInput', to: 'gotNfa' },
+    { name: 'incorrectReInput', from: 'hasReInput', to: 'waitingReInput' },
     { name: 'match', from: 'gotNfa', to: 'waitingStringToMatch' },
-    { name: 'tryAnother', from: 'gotNfa', to: 'waitingReInput' },
-    { name: 'restart', from: 'gotNfa', to: 'initial' },
-    { name: 'goto', from: '*', to: (s) => { return s; } }
+    { name: 'retry', from: 'gotNfa', to: 'waitingReInput' },
+    { name: 'restart', from: '*', to: 'initial' },
+    { name: 'stringToMatch', from: 'waitingStringToMatch', to: 'gotNfa' },
   ]
 });
 
@@ -89,35 +91,9 @@ bot.on('follow', async (event) => {
 bot.on('postback', async (event) => {
   let data = queryString.parse(event.postback.data);
   let user = activeUsers.find(u => u.id === event.source.userId);
+  let profile = await event.source.profile();
 
   if (!user) {
-    // See which action had been selected by user.
-    switch (data.action) {
-      case 're2nfa':
-        // Check if user want to do optimization.
-        user.opt = data.opt === 'true';  
-
-        let reply =
-            `${messages.askForReInput[0]}${(user.opt)? '🚀' : ''}\n` +
-            `${messages.askForReInput[1]}`;
-        event.reply(reply);
-        break;
-      case 'help':
-        event.reply('No help yet');
-        break;
-      default:
-        // May be 'match', 'retry' or 'restart'.
-        event.reply([
-          {
-            type: 'text',
-            text: `${messages.greeting.random()} ${profile.displayName} ` +
-                  messages.selectFollowing
-          },
-          messages.mainMenu
-        ]);
-        return;  // ask for a main menu option
-    }
-
     // Make a new active user.
     user = {
       id: event.source.userId,
@@ -126,13 +102,70 @@ bot.on('postback', async (event) => {
       input: ''
     }
 
-    console.log('new:', user);
-    activeUsers.push(user);
+    // See which action had been selected by user.
+    switch (data.action) {
+      case 're2nfa':
+        // Check if user want to do optimization.
+        user.opt = data.opt === 'true';  
+
+        let reply =
+            `${messages.askForReInput}${(user.opt)? '🚀' : ''}\n` +
+            `${messages.hint}`;
+        event.reply(reply);
+        break;
+      case 'help':
+        event.reply('No help yet');
+        break;
+      default:
+        // May be 'match', 'retry' or 'restart' but user is not active.
+        // So just push the main menu to user.
+        event.reply([
+          {
+            type: 'text',
+            text: `${messages.greeting.random()} ${profile.displayName} ` +
+                  messages.selectFollowing
+          },
+          messages.mainMenu
+        ]);
+        data.action = 'restart';  // modify the action
+        break;
+    }
 
     user.fsm[data.action]();  // do the transition
-  } else {
-    // User is currently in the active user list, so go to wherever they want.
-    console.log('old:', user);
+    activeUsers.push(user);
+    return;
+  }
+
+  // User is currently in the active user list and there is a transition from
+  // current state, which means it is a valid operation.
+  if (user.fsm.can(data.action)) {
+    switch (data.action) {
+      case 're2nfa':  // from main menu (initial state)
+        user.opt = data.opt === 'true';  
+      case 'retry':   // from nfa generated (gotNfa state)
+        let reply =
+            `${messages.askForReInput}${(user.opt)? '🚀' : ''}\n` +
+            `${messages.hint}`;
+        event.reply(reply);
+        break;
+      case 'help':
+        event.reply('No help yet');
+        break;
+      case 'match':
+        event.reply(askForStringToMatch);
+        break;
+      case 'restart':
+        // Remove the user from active user list and push main menu to him/her.
+        event.reply([
+          {
+            type: 'text',
+            text: messages.selectFollowing
+          },
+          messages.mainMenu
+        ]);
+      default:
+    }
+    user.fsm[data.action]();  // do the transition
   }
 });
 
@@ -153,22 +186,64 @@ bot.on('message', async (event) => {
     return;
   }
 
+  // Only text message is accepted
+  if (event.message.type !== 'text') {
+    let reply = [messages.didNotGetIt.random()];
+    switch (user.fsm.state) {
+      case 'waitingHelpType':
+        reply.push(`Please select a help type!`);
+        break;
+      case 'waitingReInput':
+        reply.push(`${messages.askForReInputAgain}${(user.opt)? ' 🚀' : ''}`);
+        break;
+      case 'waitingStringToMatch':
+        reply.push(`${messages.askForStringToMatch}`);
+        break;
+      default:
+    }
+    event.reply(reply);
+    return;
+  }
+
+  let input = event.message.text;
+
   // User is currently in the active user list.
   switch (user.fsm.state) {
     case 'waitingReInput':
-      if (event.message.type !== 'text') {
-        event.reply(messages.notTextMessage.random());
-        user.fsm.incorrectReInput();
+      user.input = input.trim();
+      event.reply([
+        {
+          type: 'text',
+          text: `💡 You entered: ${user.input}`
+        },
+        messages.confirmReInputTemplate
+      ]);
+      user.fsm.reInput();
+      break;
+    case 'hasReInput':
+      if (input === 'no') {
+        event.reply(`${messages.askForReInput}${(user.opt)? ' 🚀' : ''}`);
+        user.fsm.regret();
+        return;
+      } else if (input !== 'yes') {
+        // Don't know what user say, ask from confirmation again.
+        event.reply([
+          messages.didNotGetIt.random(),
+          messages.confirmReInputTemplate
+        ]);
         return;
       }
 
-      user.input = event.message.text;
+      // User said yes.
       try {
+        // Try compiling the RE.
         let fsm = reCompiler.compile(user.input, optimize=user.opt);
 
+        // Once success, get the dot script for plotting.
         let dotScript = reCompiler.getDotScript(fsm);
         await render(dotScript);
 
+        // Wait for uploading the image to imgur.
         let res = await upload2Imgur(OUTPUT_FILE);
         event.reply([
           {
@@ -182,21 +257,24 @@ bot.on('message', async (event) => {
           },
           messages.onNfaGeneratedMenu
         ]);
+
+        // The user can proceed to the next state.
         user.fsm.correctReInput();
       } catch (e) {
         console.log(e);
+        // @todo rander error message
         let errorMessage = reCompiler.getErrorMessage(e);
 
         // Something went wrong, ask for RE input again
         let reply = [
           (errorMessage)? errorMessage : e,
-          `${messages.askForReInputAgain[0]}${(user.opt)? '🚀' : ''}`
+          `${messages.askForReInputAgain}${(user.opt)? ' 🚀' : ''}`
         ]
         event.reply(reply);
         user.fsm.incorrectReInput();
       }
       break;
-    case 'onNfaGenerated':
+    case 'waitingStringToMatch':
       break;
     default:
       break;
